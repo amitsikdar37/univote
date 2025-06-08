38c8830b4dae495cd76c072cd89cb8dc22558696
Amit Sikdar <sikdara477@gmail.com>
2025-06-08 11:01:53 +0530

    update public claim handling to use POST method; enhance nonce generation with poseidon initialization check

--- frontend/scripts/voting.js
+++ frontend/scripts/voting.js
@@ -22,3 +22,39 @@ window.addEventListener('DOMContentLoaded', async () => {
     window.location.href = './login.html';
   }
 });
+
+const searchInput = document.getElementById('voteSearchInput');
+const searchButton = document.getElementById('electionIdSearchButton');
+
+searchButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const election_id = searchInput.value;
+
+  if (!election_id) {
+    alert("Please enter a Voting ID.");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${BACKEND_URL}/api/CheckPublicClaim`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ election_id }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Vote not found");
+    }
+
+    const voteData = await response.json();
+    console.log("Vote data:", voteData);
+    // Display vote data to the user
+  } catch (error) {
+    console.error("Error fetching vote data:", error);
+    alert("You Are Not Eligible To Registered For This Election.");
+  }
+});
